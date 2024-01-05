31c585504e1bfc81af999f1891f8443d69b05396
Pavel <pashastager@gmail.com>
2024-01-05 18:09:38 +0300

    lesson 2

--- pavel_course_aqa/src/main/java/org/example/pavel/Main.java
+++ pavel_course_aqa/src/main/java/org/example/pavel/Main.java
@@ -1,60 +1,59 @@
+package org.example.pavel;
+
 public class Main {
 
-    // Задание 1
+    // Задание 2
     public static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
     }
 
-    // Задание 2
+    // Задание 3
     public static void checkSumSign() {
-        int a = 1, b = -3;
-
+        int a = 5;
+        int b = 28;
         if (a + b >= 0) {
             System.out.println("\"Сумма положительная\"");
-        }
-        else {
+        } else {
             System.out.println("\"Сумма отрицательная\"");
         }
     }
 
-    // Задание 3
+    // Задание 4
     public static void printColor() {
-        int value = 1;
-
+        int value = -12;
         if (value <= 0) {
-            System.out.println("Красный");
+            System.out.println("\"Красный\"");
+        } else if (value <= 100) {
+            System.out.println("\"Желтый\"");
+        } else {
+            System.out.println("\"Зеленый\"");
         }
-        else if (value <= 100) {
-            System.out.println("Жёлтый");
-        }
-        else if (value > 100) {
-            System.out.println("Зелёный");
-        }
+
     }
 
-    // Задание 4
+    // Задание 5
     public static void compareNumbers() {
-        int a =3, b = 2;
+        int a = 12;
+        int b = 2;
+        if (a + b >= 0) {
+            System.out.println(a+ ">="+b);
+        } else {
+            System.out.println(a+ "<"+b);
+        }
 
-        if (a >= b) {
-            System.out.println("\"a >= b\"");
-        }
-        else {
-            System.out.println("\"a < b\"");
-        }
     }
 
     public static void main(String[] args) {
-        System.out.println("\n\tЗадание 1");
-        printThreeWords();
+        {
+            System.out.println("Задание 2:");
+            printThreeWords();
+            System.out.println("\nЗадание 3:");
+            checkSumSign();
+            System.out.println("\nЗадание 4:");
+            printColor();
+            System.out.println("\nЗадание 5:");
+            compareNumbers();
 
-        System.out.println("\n\tЗадание 2");
-        checkSumSign();
-
-        System.out.println("\n\tЗадание 3");
-        printColor();
-
-        System.out.println("\n\tЗадание 4");
-        compareNumbers();
+        }
     }
 }
